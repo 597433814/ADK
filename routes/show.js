1d65bfc4efe15029262090c545b893385055693a
zhangwei <597433814@qq.com>
2017-03-27 14:08:15 +0800

    3-27

--- routes/show.js
+++ routes/show.js
@@ -2,18 +2,29 @@ var express = require('express');
 var router = express.Router();
 var model=require('../tools/mongo.js');
 var user=model.user;
+var cart=model.cart;
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	res.render('show/cart', { admin: req.session.admin});
+	cart.find({userId:req.session.admin._id},function(err,data){
+		if(err){
+			res.end('查询失败')
+			console.log('查询失败')
+		}else{
+			console.log(data)
+			res.render('show/cart', {fuck:data, admin: req.session.admin});
+		}
+	})
 
 });
 router.post('/lists', function(req, res, next) {
 //		console.log(req.body)
-			user.update({_id:req.session.admin._id},{$set:req.body},function(err,data){
+			req.body.userId=req.session.admin._id;
+//			console.log(req.body)
+			cart.create(req.body,function(err,data){
 				if(err){
 					res.end('添加失败')
 				}else{
-					console.log(data)
+					res.redirect('show/cart')
 				}
 			})
 
--- tools/mongo.js
+++ tools/mongo.js
@@ -1,8 +1,9 @@
 var mongoose=require('mongoose');
 mongoose.connect('mongodb://localhost:27017/adk');
-var user=mongoose.model('user',{username:String,password:String,user:String,rkey:String,title:String,price:Number,img:String,num:Number,size:Number})
-var shop=mongoose.model('shop',{title:String,price:Number,img:String,makprice:Number,sex:String,num:Number})
+var user=mongoose.model('user',{username:String,password:String,user:String,rkey:String,title:String,price:Number,img:String,num:Number,size:Number});
+var shop=mongoose.model('shop',{title:String,price:Number,img:String,makprice:Number,sex:String,num:Number});
+var cart=mongoose.model('cart',{title:String,price:Number,img:String,makprice:Number,sex:String,num:Number,size:Number,userId:String});
 exports.user=user;
 exports.shop=shop;
-
+exports.cart=cart;
 
